ed38cb334c8b2c309226b70a7613ba53f87482ea
pask876 <32743027+pask876@users.noreply.github.com>
2017-12-13 14:20:27 +0100

    Update proofOfExistence.sol

--- proofOfExistence.sol
+++ proofOfExistence.sol
@@ -1,28 +1,31 @@
 pragma solidity ^0.4.15;
 
 contract ProofOfExistence{
-  bytes32[] public proofs;
+  bytes32[] private proofs;
   address public owner;
+  uint256 private lenght;
   
   // constructor
   function ProofOfExistence() public {owner = owner_();}
   
   function storeProof(bytes32 proof) private {
-    proofs.push(proof);  
+    proofs.push(proof);
+    
   } 
   
   function notarize (string document) public {
    bytes32 proof = proofFor(document);
    storeProof(proof);
+   lenght++;
   }
 
   function proofFor(string document) private pure returns (bytes32){
    return sha256(document);
   }
-  
+ 
+ // ho dovuto creare la variabile lenght dato che proofs.lenght da errore
   function checkDocument(string document) public constant returns(bool){
-      uint256 L=proofs.lenght;
-      for(uint256 i=0; i < proofs.lenght; i++) {
+       for(uint256 i=0; i < lenght; i++) {
           if (proofs[i]==proofFor(document)) {return true;}  }
           return false;
   }
